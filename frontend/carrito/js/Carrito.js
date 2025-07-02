ad395b952dfc79d61a7285f4a40291422bb642fe
arielzb1 <arielzubr@gmail.com>
2025-07-02 17:05:50 -0300

    feat: cree modulo carrito, cree y diseñe header-bot y agregue un imagen en el main

---
+++ frontend/carrito/js/Carrito.js
@@ -0,0 +1,19 @@
+export class Carrito{
+    constructor(){
+        this.productos = this.cargar();
+    }
+
+    agregar(producto){}
+    
+    eliminar(){}
+
+    vaciar(){}
+
+    mostrar(){}
+
+    guardar(){}
+
+    cargar(){}
+
+    actualizar(){}
+}
--- frontend/main/js/script.js
+++ frontend/main/js/script.js
@@ -2,12 +2,22 @@ function detectarIconoUsuario(){
     document.getElementById("icon-user").addEventListener("click",()=>{cambiarVentana("../login/login.html");});
 };
 
+function detectarIconoCarrito(){
+    document.getElementById("icon-carrito").addEventListener("click",()=>{cambiarVentana("../carrito/carrito.html");});
+}
+
+
+function detectarIconos(){
+    detectarIconoUsuario();
+    detectarIconoCarrito();
+}
+
 function cambiarVentana(direccion){
     window.location.href = direccion;
 }
 
 function init(){
-    detectarIconoUsuario();
+    detectarIconos();
 }
 
 /* Que se cargue el codigo despues de iniciar la pagina */
